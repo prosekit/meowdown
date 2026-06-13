b8d42fb6c5c6bfd4c3a5d15b7f4e0356394ff7e7
ocavue <ocavue@users.noreply.github.com>
2026-06-13 21:34:02 +1000

    refactor(react): use CSS modules (#22)

---
+++ packages/react/src/components/autocomplete-menu.module.css.d.ts
@@ -0,0 +1,7 @@
+// @ts-nocheck
+declare const styles = {
+  'Positioner': '' as string,
+  'Popup': '' as string,
+  'Item': '' as string,
+} as const;
+export default styles;
--- packages/react/src/components/drop-indicator.tsx
+++ packages/react/src/components/drop-indicator.tsx
@@ -1,5 +1,7 @@
 import { DropIndicator as ProseKitDropIndicator } from '@prosekit/react/drop-indicator'
 
+import styles from './drop-indicator.module.css'
+
 export function DropIndicator() {
-  return <ProseKitDropIndicator className="meowdown-drop-indicator" data-testid="drop-indicator" />
+  return <ProseKitDropIndicator className={styles.DropIndicator} data-testid="drop-indicator" />
 }
