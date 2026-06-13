1645d5bf0c68f00664992d634491a73662418f44
ocavue <ocavue@gmail.com>
2026-06-14 02:58:05 +1000

    fix

---
+++ packages/react/src/extensions/code-block-view.ts
@@ -0,0 +1,12 @@
+import type { Extension } from '@prosekit/core'
+import { defineReactNodeView, type ReactNodeViewComponent } from '@prosekit/react'
+
+import { CodeBlockView } from '../components/code-block-view.tsx'
+
+export function defineCodeBlockView(): Extension {
+  return defineReactNodeView({
+    name: 'codeBlock',
+    contentAs: 'code',
+    component: CodeBlockView satisfies ReactNodeViewComponent,
+  })
+}
