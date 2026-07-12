d9c49a33c12224ded042b83056b7a345e8cfed57
ocavue <ocavue@gmail.com>
2026-07-13 01:33:05 +1000

    review

--- packages/core/src/extensions/clipboard/semantic-inline.ts
+++ packages/core/src/extensions/clipboard/semantic-inline.ts
@@ -162,6 +162,7 @@ function appendTextWithBreaks(parent: HTMLElement, text: string): void {
 }
 
 // REVIEW: do not use globalThis.Node in the while codebase. Just use Node directly.
+// REVIEW: rename this function to `atomUnitToDOM`
 function atomUnitDOM(atom: Mark, sourceText: string): globalThis.Node {
   switch (atom.type.name as MarkName) {
     case 'mdImage': {
