83825ac55f59b81087708f2f7b0debe4a0a7416a
ocavue <ocavue@gmail.com>
2026-07-23 20:26:27 +1000

    feat(react): render Markdown reference links

---
+++ packages/core/src/internal.ts
@@ -0,0 +1,10 @@
+export {
+  inlineTextToMarkChunksWithContext,
+  type InlineMarkContext,
+} from './extensions/inline-text-to-mark-chunks.ts'
+export {
+  collectReferenceDefinitions,
+  type ReferenceDefinition,
+  type ReferenceDefinitionIndex,
+  type ReferenceDefinitions,
+} from './extensions/reference-links.ts'
