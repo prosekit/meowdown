51cc9edf6e5ebc5049e910ccc95ea4fe15879756
ocavue <ocavue@gmail.com>
2026-07-24 17:02:43 +1000

    fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ const coverageEnabled = !!process.env.MEOWDOWN_TEST_COVERAGE
 
 export default defineConfig({
   test: {
-    reporters: process.env.GITHUB_ACTIONS ? ['github-actions', 'verbose'] : ['default'],
+    reporters: process.env.GITHUB_ACTIONS ? ['github-actions', 'verbose', 'json'] : ['default'],
     retry: process.env.CI ? 3 : 0,
     coverage: {
       enabled: coverageEnabled,
