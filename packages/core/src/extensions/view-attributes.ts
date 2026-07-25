40f9f142cdac735c6800fd6c02590397173eeec7
ocavue <ocavue@gmail.com>
2026-07-26 04:37:44 +1000

    add type

--- packages/core/src/extensions/view-attributes.ts
+++ packages/core/src/extensions/view-attributes.ts
@@ -1,9 +1,9 @@
-import { definePlugin } from '@prosekit/core'
+import { definePlugin, type PlainExtension } from '@prosekit/core'
 import { type EditorState, Plugin, PluginKey } from '@prosekit/pm/state'
 
 export function defineViewAttributes(
   attributes: { [name: string]: string } | ((state: EditorState) => { [name: string]: string }),
-) {
+): PlainExtension {
   return definePlugin(
     new Plugin({
       key: new PluginKey('meowdown-view-attributes'),
