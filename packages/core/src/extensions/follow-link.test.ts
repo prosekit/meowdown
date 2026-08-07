7cfaf5b9d94b44bacc744a2cb7782ec0193f51f8
ocavue <ocavue@gmail.com>
2026-08-07 22:19:55 +1000

    w

--- packages/core/src/extensions/follow-link.test.ts
+++ packages/core/src/extensions/follow-link.test.ts
@@ -222,14 +222,7 @@ describe('defineFollowLinkHandler', () => {
     await userEvent.keyboard('{Enter}')
     await userEvent.keyboard('A')
 
-    expect(docToMarkdown(fixture.doc)).toMatchInlineSnapshot(`
-      """
-      see 
-
-      A here
-
-      """
-    `)
+    expect(fixture.doc.toString()).toMatchInlineSnapshot(`"doc(paragraph("see "), paragraph("A here"))"`)
   })
 
   it('Enter on a selected wikilink inside a list item does not split the item', async () => {
