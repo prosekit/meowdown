0e60cb76f45a618f910aea6281a4723484b5f464
ocavue <ocavue@gmail.com>
2026-08-14 20:32:17 +1000

    add docs

--- packages/core/src/vite-env.d.ts
+++ packages/core/src/vite-env.d.ts
@@ -1,3 +1,5 @@
+// https://github.com/vitejs/vite/blob/v8.0.0/docs/guide/env-and-mode.md?plain=1#L125-L146
+
 interface ViteTypeOptions {
   // By adding this line, you can make the type of ImportMetaEnv strict
   // to disallow unknown keys.
