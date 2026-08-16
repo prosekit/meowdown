7ddd12b644dca9ac47303c93ca077ea7e83a1952
ocavue <ocavue@gmail.com>
2026-08-16 16:23:31 +1000

    fix

--- packages/core/src/converters/check-roundtrip.test.ts
+++ packages/core/src/converters/check-roundtrip.test.ts
@@ -259,6 +259,9 @@ const NORMALIZING_CASES: string[] = [
 
   // a task with an HTML comment in its lazy line
   '- [x] <!--\nABC\n-->',
+
+  // a task with an HTML element
+  '- [x] <div></div>\nABC',
 ]
 
 const LOSSY_CASES: string[] = [
