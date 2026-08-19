465d479d334839a0a1169eb255c5b53e8dc54e21
ocavue <ocavue@gmail.com>
2026-08-19 15:13:24 +1000

    review

--- website/src/stories/task-list.stories.ts
+++ website/src/stories/task-list.stories.ts
@@ -4,4 +4,6 @@ export default {
   component: TaskListComponent,
 }
 
+// REVIEW: you have 3 *.stories.ts files. Each of them now has one single story. I do not want it. I want one single stories.ts file that contains all 3 stories.
+
 export const TaskList = {}
