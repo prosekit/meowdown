78eced21d95ef902adc5032cbc6a4e24cd343f39
ocavue <ocavue@gmail.com>
2026-08-21 16:57:24 +1000

    review

--- packages/core/src/utils/is-after-line-break.ts
+++ packages/core/src/utils/is-after-line-break.ts
@@ -1,5 +1,8 @@
 import type { ResolvedPos } from '@prosekit/pm/model'
 
+// REVIEW: FIXME: 1. rename function "isAfterLineBreak_v2_tmp" to isAfterLineBreak
+// REVIEW: FIXME: 2. rename this file from  is-after-line-break.ts to link-break.ts
+// REVIEW: FIXME: 3. merge the latest origin/master and git push
 export function isAfterLineBreak_v2_tmp($pos: ResolvedPos): boolean {
   const { parentOffset, parent } = $pos
   return parentOffset > 0 && parent.textBetween(parentOffset - 1, parentOffset) === '\n'
