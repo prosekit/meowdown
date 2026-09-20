44ab9579f8080866231875cee57e8451607cd490
ocavue <ocavue@gmail.com>
2026-09-21 05:40:19 +1000

    refactor(embed): remove declarations with no effect

--- packages/embed/src/x/features.test.tsx
+++ packages/embed/src/x/features.test.tsx
@@ -50,17 +50,6 @@ describe('Full post snapshots', () => {
     expect(box.height).toBeCloseTo((box.width * 400) / 640, 0)
   })
 
-  it('shows a portrait photo whole at the thumbnail height', () => {
-    const snapshot = createPost()
-    snapshot.media = [{ ...createPhoto(), width: 600, height: 1200 }]
-    const element = mount(snapshot)
-    const item = element.querySelector('[data-media-item]')!.getBoundingClientRect()
-    const image = element.querySelector('[data-media] img')!.getBoundingClientRect()
-    expect(item.height).toBeCloseTo(7.5 * 16, 0)
-    expect(image.height).toBeCloseTo(item.height, 0)
-    expect(image.width).toBeCloseTo(image.height / 2, 0)
-  })
-
   it('grows a video that plays in place', async () => {
     const snapshot = createPost()
     snapshot.media = [createVideo()]
